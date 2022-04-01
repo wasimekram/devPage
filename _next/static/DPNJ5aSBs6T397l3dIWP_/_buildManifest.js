a6c7319df4cb12f33e1b0d6cd3a8da3bf62a3b64
wasimekram <40612830+wasimekram@users.noreply.github.com>
2022-04-01 18:48:33 +0000

    Deploying to gh-pages from @ wasimekram/devPage@1f0ed4086e5aec0e3154a86e02c5d7677ecefca1 🚀

---
+++ _next/static/DPNJ5aSBs6T397l3dIWP_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-91b80ce9cda3124b.js","static/chunks/68-ba29963614a76192.js","static/chunks/pages/index-1df3b6d3ac5990c6.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
